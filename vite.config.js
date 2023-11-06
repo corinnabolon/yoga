c49d723bc59ff6489a7e0bca937e46e5aa91f06c
Corinna Bolon <kyndrill@rocketmail.com>
2023-11-06 11:19:51 -0700

    testing another base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '',
+  base: ' ',
   build: {
     outDir: 'docs',
     sourcemap: false
