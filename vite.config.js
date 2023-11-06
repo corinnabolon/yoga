aa7c6050aea2ddc72eb76bd0c91e120a1fb972b5
Corinna Bolon <kyndrill@rocketmail.com>
2023-11-06 11:23:11 -0700

    yoga build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: ' ',
+  base: 'yoga',
   build: {
     outDir: 'docs',
     sourcemap: false
